69d7eabd9b7708fb0f739574073a70727d8be17d
NiNickle <36533875+NiNickle@users.noreply.github.com>
2020-04-12 13:11:13 +0700

    Update 6234422823-Lab2_6.py

--- lab 2/6234422823-Lab2_6.py
+++ lab 2/6234422823-Lab2_6.py
@@ -2,5 +2,5 @@
 #Nichaphat Chawananukul 6234422823
 
 name=input("Name : ")
-TF=str(" " in name or "." in name)
-print ("'"+name+"'","contains blank or period: "+TF)
+#TF=str(" " in name or "." in name)
+print ("'"+name+"'","contains blank or period: "+str(" " in name or "." in name))
